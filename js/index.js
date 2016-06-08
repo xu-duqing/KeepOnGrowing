974830d98f356e0a8d84658d39ac3fc7a3bd34bf
许度庆 <xu.duqing@gmail.com>
2016-06-08 16:09:08 +0800

    add 接入parse

--- js/index.js
+++ js/index.js
@@ -12,6 +12,8 @@ import React,{
 import Root from './App';
 var { Provider } = require('react-redux');
 var configureStore = require('./store/configureStore');
+var Parse = require('parse/react-native');
+var { serverURL,parseAppID } = require('./env');
 
 
 export default class App extends React.Component{
@@ -20,6 +22,27 @@ export default class App extends React.Component{
     constructor(props) {
         super(props);
         // 初始状态
+        Parse.initialize(parseAppID);
+        Parse.serverURL = `${serverURL}/parse`;
+
+        //用户注册
+        //let user = new Parse.User();
+        //user.set("username","SamXu");
+        //user.set("password","111111");
+        //user.set("email","xu.duqing@gmail.com");
+        //user.set("phone","18268839400");
+        //
+        //user.signUp(null,{
+        //    success:function(user){
+        //        console.log(user)
+        //    },
+        //    error:function(user,error){
+        //        console.log(error);
+        //        console.log(user);
+        //        alert("Error: " + error.code + " " + error.message);
+        //    }}
+        //)
+
         this.state = {
           isLoading: true,
           store: configureStore(() => this.setState({isLoading: false})),
