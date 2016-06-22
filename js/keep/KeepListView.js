e88d8fe6e275a950408a0e337988e33008de745d
许度庆 <xu.duqing@gmail.com>
2016-06-23 00:13:30 +0800

    fix 去除不需要的代码

--- js/keep/KeepListView.js
+++ js/keep/KeepListView.js
@@ -8,7 +8,6 @@ import React,{
 
 import ListItem from './KeepListView.Item';
 import Header from './KeepListView.Header';
-import {getDayTime} from '../utils/date_utils';
 import * as KGColor from 'KGColor'
 
 var ds = new ListView.DataSource({rowHasChanged: (row1, row2) => row1 !== row2, sectionHeaderHasChanged: (prev, next) => prev !== next});
@@ -22,25 +21,6 @@ export default class KeepListView extends React.Component{
         this.state = {
             keeps:[]
         };
-    }
-
-    keepsToDateSource(keeps){
-        keeps.sort((keep1,keep2) => {
-                return keep2.time - keep1.time
-            });
-
-        let dataSource ={};
-        keeps.forEach((item =>{
-            const keyDay = getDayTime(item.time);
-            if (dataSource[keyDay]){
-                //fixme 暂时不做处理
-            }else {
-                dataSource[keyDay] = [];
-            }
-
-            dataSource[keyDay].push(item);
-        }));
-        return dataSource
     }
 
     componentWillReceiveProps(nextProps) {
