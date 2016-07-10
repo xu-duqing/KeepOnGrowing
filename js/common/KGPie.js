02910906417081af49568d01921bfb0b3360e90f
许度庆 <xu.duqing@gmail.com>
2016-07-10 15:30:59 +0800

    fix pie

--- js/common/KGPie.js
+++ js/common/KGPie.js
@@ -7,45 +7,40 @@
 import React from 'react'
 
 import {
-    View
+    View,
+    StyleSheet
 } from 'react-native'
-
-import Svg, {
-    Circle,
-    Ellipse,
-    G,
-    LinearGradient,
-    RadialGradient,
-    Line,
-    Path,
-    Polygon,
-    Polyline,
-    Rect,
-    Symbol,
-    Text,
-    Use,
-    Defs,
-    Stop
-} from 'react-native-art-svg';
+import Chart from 'react-native-chart';
 
 
 export default class KGPie extends React.Component{
 
+
+
     render(){
+        const data = [
+            [0, 1],
+            [1, 3],
+            [3, 7],
+            [4, 9]
+        ];
+
         return(
-            <View>
-                <Svg
-                    height="100"
-                    width="110"
-                >
-                    <Circle
-                        cx="50"
-                        cy="50"
-                        r="50"
-                        fill="pink"
-                    />
-                </Svg>
+            <View style={styles.container}>
+                <Chart
+                    style={styles.chart}
+                    data={data}
+                    verticalGridStep={5}
+                    type="pie"
+                />
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container:{
+        width:100,
+        height:100
+    }
+});
